a64417838892923401fb8eeeee9ee67d942a981e
Frank Albenesius <frankalbenesius@gmail.com>
2017-07-23 16:23:15 -0500

    add favicon

--- pages/_document.js
+++ pages/_document.js
@@ -13,6 +13,7 @@ export default class MyDocument extends Document {
       <html>
         <Head>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="/static/img/favicon.ico" />
         </Head>
         <body className="custom_class">
           {this.props.customValue}
