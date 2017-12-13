655ca7c7092e27d94b7a4252bd401eb3968cc4c7
Frank Albenesius <frankalbenesius@gmail.com>
2017-12-13 10:48:39 -0600

    expand projects list

--- pages/projects.js
+++ pages/projects.js
@@ -1,6 +1,6 @@
-import Head from 'next/head';
-import Link from 'next/link';
-import Page from '../components/Page';
+import Head from 'next/head'
+import Link from 'next/link'
+import Page from '../components/Page'
 
 export default () => (
   <Page>
@@ -8,31 +8,52 @@ export default () => (
       <title>Projects</title>
     </Head>
     <div className="project">
-      <a target="_blank" href="https://table.dire.tools">Dire Table</a>
+      <a target="_blank" href="https://spinner.albenesi.us">
+        Spinner
+      </a>
       <p>
-        A minimalistic virtual tabletop for tabletop role-playing games.
+        A simple spinner created for a teacher friend of mine who wanted a way
+        to randomly choose students without leaving any students unpicked for
+        too long.
       </p>
+    </div>
+    <div className="project">
+      <a target="_blank" href="https://spinner.albenesi.us">
+        Snake
+      </a>
+      <p>A game of snake built with vanilla JavaScript!</p>
+    </div>
+    <div className="project">
+      <a target="_blank" href="https://table.albenesi.us">
+        Dire Table
+      </a>
+      <p>A minimalistic virtual tabletop for tabletop role-playing games.</p>
     </div>
     <div className="project">
       <a target="_blank" href="https://spotify.albenesi.us">
         Spotify Top Tracks
       </a>
-      <p>
-        A small tool to review your top-played Spotify tracks.
-      </p>
+      <p>A small tool to review your top-played Spotify tracks.</p>
     </div>
     <div className="project">
-      <Link prefetch href="/bagels"><a>Homemade Bagels</a></Link>
-      <p>
-        They're delicious.
-      </p>
+      <Link prefetch href="/bagels">
+        <a>Homemade Bagels</a>
+      </Link>
+      <p>They're delicious.</p>
+    </div>
+    <div className="project">
+      <a target="_blank" href="https://spotify.albenesi.us">
+        popogigio.com
+      </a>
+      <p>Because the internet still needs weird stuff.</p>
     </div>
 
     <style jsx>{`
       .project {
         padding: 1rem;
         text-align: center;
-        margin-bottom: 1rem;
+        max-width: 30em;
+        margin: 0 auto;
       }
       .project a {
         display: inline-block;
@@ -40,4 +61,4 @@ export default () => (
       }
     `}</style>
   </Page>
-);
+)
